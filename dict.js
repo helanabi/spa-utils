b830bc6a7e854e231b2398b2f695ccb187efb233
Hassan El Anabi <helanabi@gmail.com>
2023-01-07 13:01:36 +0100

    add dict.js

---
+++ dict.js
@@ -0,0 +1,24 @@
+export default function makeDict(defaultLanguage, translations) {
+    let lang = localStorage.getItem("lang") ?? defaultLanguage;
+
+    for (const translation of Object.values(translations)) {
+            translation.node = new Text(translation[lang]);
+    }
+
+    return {
+	dict(phraseAlias) {
+	    return translations[phraseAlias].node;
+	},
+
+	setLang(newLang) {
+	    if (lang === newLang) return;
+	    
+            lang = newLang;
+            localStorage.setItem("lang", newLang);
+
+	    for (const translation of Object.values(translations)) {
+                translation.node.textContent = translation[lang];
+            }
+	}
+    };
+}
